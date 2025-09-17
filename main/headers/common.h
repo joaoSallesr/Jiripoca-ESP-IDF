367f6dbb1aab2ca7c7ff9094eabc385e93845fb3
Eduardo Mateus <edumwteus@gmail.com>
2025-09-17 16:41:30 -0300

    after review changes

--- main/headers/common.h
+++ main/headers/common.h
@@ -24,7 +24,6 @@
 #include "esp_log.h"
 #include "esp_err.h" 
 #include "esp_timer.h"
-#include "esp_mac.h" //revisar
 
 #include "freertos/queue.h"
 #include "freertos/semphr.h"
@@ -35,8 +34,8 @@
 #define LED_GPIO 15
 #define ALED_GPIO 6
 #define BUTTON_GPIO 0
-#define DROGUE_GPIO 47 //48?
-#define MAIN_GPIO 48 //47?
+#define DROGUE_GPIO 48
+#define MAIN_GPIO 47
 #define RBF_GPIO 4
 #define GPS_RX 21
 #define I2C_SCL 9
