0ebd51edfa84b453ed844f1d896626dfcf82c273
joaoSallesr <joao.sallesr@gmail.com>
2025-09-22 19:18:12 -0300

    Commit Fix

--- main/headers/acquire.h
+++ main/headers/acquire.h
@@ -8,6 +8,7 @@
 
 #define G 9.80665
 
+
 #define FLYING_THRESHOLD 10 * G // Acceleration threshold to consider rocket flying
 #define CUTOFF_THRESHOLD 3 * G // Acceleration threshold to consider motor cutoff
 #define LANDED_THRESHOLD 2     // Altitude threshold to consider rocket landed
